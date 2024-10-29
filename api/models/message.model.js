6eba8a3b4b118786d01985988558c65d5780fec4
mohammadalnseirat <146799067+mohammadalnseirat@users.noreply.github.com>
2024-10-29 11:25:11 +0400

    create User model

---
+++ api/models/message.model.js
@@ -0,0 +1,6 @@
+import mongoose from "mongoose";
+
+const messageSchema = new mongoose.Schema({}, { timestamps: true });
+
+const Message = mongoose.model("Message", messageSchema);
+export default Message;
